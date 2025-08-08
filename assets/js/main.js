fded079502c6ad99ede1719a4c3eae0080f4108f
am-mahmud <am.mahmudasif@gmail.com>
2025-08-08 14:16:00 -0400

    New design start

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// // Navbar small screen 
+//   const hamburgerButton = document.getElementById('hamburger-button');
+//   const navMenu = document.getElementById('navbar-hamburger');
+
+//   hamburgerButton.addEventListener('click', () => {
+//     navMenu.classList.toggle('hidden');
+//   });
